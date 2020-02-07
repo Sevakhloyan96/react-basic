f53efe73026cce3c254c33a576bc381677076803
Sevakhloyan96 <Sevak.hloyan.96@mail.ru>
2020-02-08 00:40:23 +0400

    map example

--- src/app/content/comment/component.jsx
+++ src/app/content/comment/component.jsx
@@ -10,18 +10,44 @@ function formatDate(date) {
     return met
 } 
 
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo author={props.user.author} />
-            <div className="Comment-text">
-                {props.user.text}
+class Comment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbers: [1, 2, 3, 5, 6, 7, 4, 9]
+        }
+    }
+
+   foo = (num) => {
+        let met = num.map((item, index) => {
+            return (
+                <li key={index} >
+                    <span>{item}</span>
+                </li>
+            )
+        })
+        console.log(met);
+        return met
+    }
+
+    render() {
+        return (
+            <div className="Comment">
+                <UserInfo author={this.props.user.author} />
+                <div className="Comment-text">
+                    {this.props.user.text}
+                </div>
+                <div className="Comment-date">
+                    {formatDate(this.props.user.date)}
+                </div>
+                <ul>
+                    {this.foo(this.state.numbers)}
+                </ul>
             </div>
-            <div className="Comment-date">
-                {formatDate(props.user.date)}
-            </div>
-        </div>
-    );
+       
+        );
+    }
 }
 
-export default Comment;
+
+export default Comment
