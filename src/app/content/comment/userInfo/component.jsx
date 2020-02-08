ea146f0290b9ef99e7dface1953a49b3227a7d65
RockStar <gagik.aproyan.dev@gmail.com>
2020-02-08 12:45:50 +0400

    form with textarea

--- src/app/content/comment/userInfo/component.jsx
+++ src/app/content/comment/userInfo/component.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 
 import './component.css';
 
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <img className="Avatar"
-                src={props.author.avatarUrl}
-                alt={props.author.name}
-            />
-            <div className="UserInfo-name">
-                {props.author.name}
+class UserInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        console.log(this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="UserInfo">
+                <img className="Avatar"
+                    src={this.props.author.avatarUrl}
+                    alt={this.props.author.name}
+                />
+                <div className="UserInfo-name">
+                    {this.props.author.name}
+                </div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <textarea value={this.state.value} onChange={this.handleChange}></textarea>
+                    </label>
+                    <button type='submit'>send</button>
+                </form>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default UserInfo;
